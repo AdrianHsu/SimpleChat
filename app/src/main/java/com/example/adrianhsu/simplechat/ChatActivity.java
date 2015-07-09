04c55c5699af7872e898de0d52c1f5b3cf43120f
AdrianHsu <adrianhsu1995@gmail.com>
2015-07-09 16:51:25 +0800

    Verify save

--- app/src/main/java/com/example/adrianhsu/simplechat/ChatActivity.java
+++ app/src/main/java/com/example/adrianhsu/simplechat/ChatActivity.java
@@ -5,12 +5,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.parse.ParseUser;
 import com.parse.ParseAnonymousUtils;
 import com.parse.LogInCallback;
+import com.parse.SaveCallback;
 import com.parse.ParseException;
+import com.parse.ParseObject;
 
 
 
@@ -18,6 +25,11 @@ public class ChatActivity extends Activity {
 
     private static final String TAG = ChatActivity.class.getName();
     private static String sUserId;
+
+    public static final String USER_ID_KEY = "userId";
+
+    private EditText etMessage;
+    private Button btSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +46,33 @@ public class ChatActivity extends Activity {
     // Get the userId from the cached currentUser object
     private void startWithCurrentUser() {
         sUserId = ParseUser.getCurrentUser().getObjectId();
+        setupMessagePosting();
+    }
+    // Setup button event handler which posts the entered message to Parse
+    private void setupMessagePosting() {
+        // Find the text field and button
+        etMessage = (EditText) findViewById(R.id.etMessage);
+        btSend = (Button) findViewById(R.id.btSend);
+        // When send button is clicked, create message object on Parse
+        btSend.setOnClickListener(new OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                String data = etMessage.getText().toString();
+                ParseObject message = new ParseObject("Message");
+                message.put(USER_ID_KEY, sUserId);
+                message.put("body", data);
+                message.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Toast.makeText(ChatActivity.this, "Successfully created message on Parse",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                etMessage.setText("");
+            }
+        });
     }
 
     // Create an anonymous user using ParseAnonymousUtils and set sUserId
