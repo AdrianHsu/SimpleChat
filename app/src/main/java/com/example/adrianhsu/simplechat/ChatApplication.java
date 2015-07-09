e3b1c4b1628cc2e967dfba436d0e89958177b16c
AdrianHsu <adrianhsu1995@gmail.com>
2015-07-09 17:30:34 +0800

    8. Create Model Class done

--- app/src/main/java/com/example/adrianhsu/simplechat/ChatApplication.java
+++ app/src/main/java/com/example/adrianhsu/simplechat/ChatApplication.java
@@ -1,10 +1,12 @@
 package com.example.adrianhsu.simplechat;
-import android.app.Application;
-import com.parse.Parse;
 
 /**
  * Created by AdrianHsu on 15/7/9.
  */
+import android.app.Application;
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 public class ChatApplication extends Application {
 
     public static final String YOUR_APPLICATION_ID = "waWZC7akbFATZdNVVQoVwZFQTGhjA6EtKrNSKkfu";
@@ -13,6 +15,10 @@ public class ChatApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Parse.enableLocalDatastore(this);
+        // Register your parse models here
+        ParseObject.registerSubclass(Message.class);
+        // Existing initialization happens after all classes are registered
         Parse.initialize(this, YOUR_APPLICATION_ID, YOUR_CLIENT_KEY);
     }
+
 }
